bd41995a75dfab2e4ef6f9cfeb771435594f2688
TedisAgolli <tedisagolli@gmail.com>
2020-12-26 15:26:01 -0500

    VS code extension

---
+++ extension.js
@@ -0,0 +1,49 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+const hexToTailwind = require("hex-to-tailwind");
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "hex-to-tailwind" is now active!'
+  );
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "hex-to-tailwind.hex-to-tailwind",
+    function () {
+      vscode.window
+        .showInputBox({ prompt: "Hex Color", placeHolder: "#FFFFFF" })
+        .then((hexInput) => {
+          const tailwindColor = hexToTailwind(hexInput).tailwind;
+          if (tailwindColor) {
+            vscode.env.clipboard.writeText(tailwindColor);
+            vscode.window.showInformationMessage(
+              `Copied ${tailwindColor} to clipboard!`
+            );
+          }
+        });
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
